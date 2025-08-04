d119f234d20f97b74e9ca59c4aff190e99f6a17d
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 11:23:00 +0000

    yg

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""
+This script checks a Markdown file and outputfile arguments.
+"""
 import sys
 import os
-if len(sys.argv) < 2:
-    sys.stder.write("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.stder.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
 
 md_file=sys.argv(1)
 
-if not os.path.isfile(md_file):
-    sys.stder.write(f"Missing {md_file} /n")
-    sys.exit(1)
+    if not os.path.isfile(md_file):
+        sys.stder.write(f"Missing {md_file} /n")
+        sys.exit(1)
 sys.exit(0)
