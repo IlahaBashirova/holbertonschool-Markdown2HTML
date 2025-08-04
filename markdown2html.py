227a9e52e06a896a57159056a0cdd6bf79c6b2e8
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 10:48:31 +0000

    tijn

---
+++ markdown2html.py
@@ -0,0 +1,12 @@
+import sys
+import os
+if len(sys.argv) < 2:
+    sys.stder.write("Usage: ./markdown2html.py README.md README.html")
+    sys.exit(1)
+
+md_file=sys.argv(1)
+
+if not os.path.isfile(md_file):
+    sys.stder.write(f"Missing {md_file} /n")
+    sys.exit(1)
+sys.exit(0)
