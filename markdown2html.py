671ed9c75e4b7213a7129e6ec2edbdfcd410afd4
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 11:36:01 +0000

    ru

--- markdown2html.py
+++ markdown2html.py
@@ -7,12 +7,12 @@ import sys
 import os
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        sys.stder.write("Usage: ./markdown2html.py README.md README.html")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
 
     md_file=sys.argv(1)
 
     if not os.path.isfile(md_file):
-        sys.stder.write(f"Missing {md_file} /n")
+        sys.stderr.write(f"Missing {md_file} /n")
         sys.exit(1)
     sys.exit(0)
