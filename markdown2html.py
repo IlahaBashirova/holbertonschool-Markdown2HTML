3eb9da217bd14e4d71ad8803157a00e8228e1dc6
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 11:24:37 +0000

    tijn

--- markdown2html.py
+++ markdown2html.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         sys.stder.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
 
-md_file=sys.argv(1)
+    md_file=sys.argv(1)
 
     if not os.path.isfile(md_file):
         sys.stder.write(f"Missing {md_file} /n")
         sys.exit(1)
-sys.exit(0)
+    sys.exit(0)
