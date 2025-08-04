d0cf6b85bfe205731deff3722881456b343eddc3
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 11:39:17 +0000

    hcb

--- markdown2html.py
+++ markdown2html.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
 
-    md_file=sys.argv(1)
+    md_file=sys.argv[1]
 
     if not os.path.isfile(md_file):
         sys.stderr.write(f"Missing {md_file} /n")
