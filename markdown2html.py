b6bb563b399925ebfbc99b0f2830d03d0de58379
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 13:38:48 +0000

    jgv

--- markdown2html.py
+++ markdown2html.py
@@ -15,4 +15,17 @@ if __name__ == "__main__":
     if not os.path.isfile(md_file):
         sys.stderr.write(f"Missing {md_file}\n")
         sys.exit(1)
+    for line  in md_file:
+        if line.startswith("#"):
+            print(<h1>line[1::]</h1>)
+        elif  line.startswith("##"):
+            print(<h2>line[2::]</h2>)
+        elif line.startswith("###"):
+            print(<h3>line[3::]</h3>)
+        elif  line.startswith("####"):
+            print(<h4>line[4::]</h4>)
+        elif  line.startswith("#####"):
+            print(<h5>line[5::]</h5>)
+        elif line.startswith("######"):
+            print(<h6>line[6::]</h6>)
     sys.exit(0)
