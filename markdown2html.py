318576a48bd47456637c772dae2ac3ef189ec8b6
IlahaBashirova <11861@holbertonschool.com>
2025-08-04 12:45:40 +0000

    fje

--- markdown2html.py
+++ markdown2html.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     if len(sys.argv) <3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
         sys.exit(1)
-
+        
     md_file=sys.argv[1]
-
+    output_file=sys.argv[2]
     if not os.path.isfile(md_file):
         sys.stderr.write(f"Missing {md_file} \n")
         sys.exit(1)
