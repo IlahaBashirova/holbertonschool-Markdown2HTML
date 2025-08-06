5b866bd22c14b85312cfbe9e50a4022d00f974be
IlahaBAshirova <11861@holbertonstudents.com>
2025-08-06 16:01:59 +0400

    eugf

--- markdown2html.py
+++ markdown2html.py
@@ -16,24 +16,52 @@ if __name__ == "__main__":
         sys.stderr.write(f"Missing {md_file}\n")
         sys.exit(1)
     with open (md_file, "r") as md, open (output_file, "w") as html:
-        li=[]
-        in_li=False
+        is_line=False
+        ol_line=False
         for line in md:
             line=line.strip()
             count=0
             for i in line:
                 if i=="#":
                     count+=1
-        if line.startswith("- "):
-            html.write("<ul>\n")
-            in_li=True
-            if in_li==True:
-                html.write(line[2::])
-            if in_li==False:
-                html.write("<ul>\n")
+                else:
+                    break
+
 
             if 1<=count<=6 and line[count:count+1]==" ":
+                if is_line:
+                    html.write(f"</ul>\n")
+                    is_line=False
+                if ol_line:
+                    html.write(f"</ol>\n")
+                    ol_line=False
+
                 content=line[count::]
                 html.write(f" <h{count}> {content} </h{count}>\n")
+
+            elif line.startswith("- "):
+                if not is_line:
+                    html.write(f"<ul>\n")
+                    is_line=True
+                html.write(f"<li>{line[2::]}</li>\n")
+            elif line.startswith("* "):
+                if not ol_line:
+                    html.write(f"<ol>\n")
+                    ol_line=True
+                html.write(f"<li>{line[2::]}</li>\n")
+            else:
+                if ol_line:
+                    html.write(f"</ol>\n")
+                    ol_line=False
+                if is_line:
+                    html.write(f"</ul>\n")
+                    is_line=False
+
+        if is_line:
+            html.write(f"</ul>\n")
+        if ol_line:
+            html.write(f"</ol>\n")
+
     
     sys.exit(0)
+    
